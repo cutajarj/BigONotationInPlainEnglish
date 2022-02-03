6d293a8500c6f360a0d96fe298a000824c90c388
cutajarj <james.cutajar@gmail.com>
2022-02-03 08:53:34 +0000

    Refactoring the closest pair quadratic to be more reader friendly.

--- quadratic/closest_pair.py
+++ quadratic/closest_pair.py
@@ -10,11 +10,11 @@ def find_closest_pair(points):
     for i in range(len(points)):
         for j in range(len(points)):
             distance = dist(points[i], points[j])
-            if i != j and distance < minimum_dist:
-                closest_pair = (points[i], points[j])
+            if distance < minimum_dist and i != j:
                 minimum_dist = distance
+                closest_pair = (points[i], points[j])
     return closest_pair
 
 
-inputs = [Point2D(10, 10), Point2D(100, 120), Point2D(50, 21), Point2D(100, 10), Point2D(60, 35), Point2D(65, 36)]
-print(find_closest_pair(inputs))
+# inputs = [Point2D(10, 10), Point2D(100, 120), Point2D(50, 21), Point2D(100, 10), Point2D(60, 35), Point2D(65, 36)]
+# print(find_closest_pair(inputs))
